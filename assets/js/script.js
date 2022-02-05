899c753546fb15c6a5275c6ac43eb83cd7a1f651
Oscar Troncoso <oscarleandro1595@gmail.com>
2022-02-05 00:54:01 -0300

    subida de pokedex

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+$(()=>{
+
+    $('form').submit(event =>{
+        event.preventDefault();
+
+        let valueInput = $('#pokemonInput').val();
+        
+        $.ajax({
+            url: 'https://pokeapi.co/api/v2/pokemon/' + valueInput,
+            success: (data)=>{
+
+                let nombre = data.name;
+                let imagen = data.sprites.front_default;
+                let peso = data.weight;
+
+                $('#pokeInfo').html(`
+
+                    <div class="text-center">
+                        <h3>${nombre}</h3>
+                        <img src="${imagen}" class='img-fluid' alt="">
+                        <h6>Peso: ${peso}</h6>
+                    </div>
+                
+                
+                
+                `)
+                let estadisticas = [];
+                data.stats.forEach((s)=>{
+                    estadisticas.push({
+                        label: s.stat.name,
+                        y: s.base_stat
+                    });
+                });
+
+                let config = {
+                    animationEnabled : true,
+                    title:{
+                        text: 'Estadisticas',
+                    },
+                    axisY:{
+                        title: 'Valor',
+                    },
+                    axisX:{
+                        title: 'Estadistica',
+                    },
+                    data:[
+                        {
+                            type: 'column',
+                            dataPoints: estadisticas,
+                        },
+                    ],
+                };
+
+                let chart = new CanvasJS.Chart('pokeStats', config);
+
+                chart.render();
+
+
+
+            },
+        });
+
+    });
+});
